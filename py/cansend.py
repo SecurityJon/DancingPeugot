bf441c55201b0f5fdb27d161d0c3de6b97b15332
SecurityJon <jonaubrey@hotmail.com>
2019-07-08 12:06:47 +0100

    Tweaks

--- py/cansend.py
+++ py/cansend.py
@@ -32,7 +32,7 @@ def carData(dataArray):
             #sys.stdout.write("\r\n")
 	    sys.stdout.write(str(canvalues[arrayNumber]))
             sys.stdout.write("\t\t")
-            sys.stdout.write("\r\n")
+        sys.stdout.write("\r\n")
 		
 
 	#########################################################
